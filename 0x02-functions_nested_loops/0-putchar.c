0f6cf85d0f38c38c2f67090bced43850bf55351a
Joshbby5 <okechukwujnr5@gmail.com>
2022-10-19 09:03:16 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main-program entry point.
+ * Return:0 non error, no error
+ */
+
+int main(void)
+{
+	char we[8] = "_putchar";
+	int x;
+
+	for (x = 0; x < 8; x++)
+{
+	_putchar(we[x]);
+}	_putchar('\n');
+}	return (0);
