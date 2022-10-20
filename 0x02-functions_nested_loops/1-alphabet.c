714ccf4ec94bccb97a560896f9f5ea51de173d31
Joshua <okechukwujnr5@gmail.com>
2022-10-20 03:47:49 -0700

    Task Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+/**
+ * Description: print_alphabet - a function that prints alphabet in lowercase,
+ * followed by a new line.
+ * Return: Nothing
+ */
+
+void print_alphabet(void)
+{
+	char x;
+
+	for (x='a'; x<='z'; x++)
+{
+	_putchar(x);
+}	_putchar('/n');
+}
+
