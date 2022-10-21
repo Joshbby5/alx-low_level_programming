cd5bb50b35a4c2c1a684a87799d5c3e03763528f
Joshua <okechukwujnr5@gmail.com>
2022-10-20 19:14:48 -0700

    2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -5,8 +5,9 @@
  * mul - multiply 2 integers
  * @a: int type number
  * @b: int type number
- * Return: result of multiplication
+ * Return:0 result of multiplication
  */
+
 int mul(int a, int b)
 {
 		return (a * b);
